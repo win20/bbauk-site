aa26191d443e77c1604d72df0a773eccba45d93b
Win Barua <wj.barua@gmail.com>
2025-06-24 00:13:47 +0100

    feat(updates-section): setup component for section (#2)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import HeroSection from "@components/hero-section";
 import ProgramsAndServicesSection from "@components/programs-and-services-section";
+import UpdatesSection from "@components/updates-section";
 
 export default function Home() {
   return (
     <div className="min-h-screen font-pt-sans">
       <HeroSection />
       <ProgramsAndServicesSection />
+      <UpdatesSection />
     </div>
   );
 }
