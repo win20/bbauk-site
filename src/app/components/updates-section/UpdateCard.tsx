f189532265b9546d377de4df0b7dd229c21a7242
Win Barua <wj.barua@gmail.com>
2025-06-24 00:00:39 +0100

    feat(component): create UpdateCard component (#2)

---
+++ src/app/components/updates-section/UpdateCard.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import { RiArrowRightSLine } from "react-icons/ri";
+import CroppedImage from "@components/shared/CroppedImage";
+
+interface UpdateCardProps {
+  id: number;
+  title: string;
+  description: string;
+  image_src: string;
+}
+
+export default function UpdateCard({ id, title, description, image_src }: UpdateCardProps) {
+  return (
+    <div className="mb-10">
+      <CroppedImage
+        src={image_src}
+        alt="Hero Image"
+        height="h-[220px]"
+        className="mt-6 rounded-md"
+        objectPosition="60%"
+        priority
+      />
+      <h4 className="mt-4 mb-2 font-merriweather text-lg">{title}</h4>
+      <p>{description}</p>
+      <Link href={`/updates/${id}`} className="mt-4 inline-flex items-center">
+        Read more
+        <RiArrowRightSLine className="mt-1 ml-1" strokeWidth={1} />
+      </Link>
+    </div>
+  );
+}
