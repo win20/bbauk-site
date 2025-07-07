007c988aeeb5e914c46152e4b8dabe24ab046250
Win Barua <wj.barua@gmail.com>
2025-07-07 23:18:00 +0100

    chore: run prettier formatting (#3)

--- src/components/shared/HeadingWithTag.tsx
+++ src/components/shared/HeadingWithTag.tsx
@@ -7,7 +7,7 @@ export default function HeadingWithTag({ heading, tag }: Props) {
   return (
     <>
       <span className="text-base font-bold">{tag}</span>
-      <h2 className="mb-4 mt-2 font-merriweather text-4xl">{heading}</h2>
+      <h2 className="mt-2 mb-4 font-merriweather text-4xl">{heading}</h2>
     </>
   );
 }
