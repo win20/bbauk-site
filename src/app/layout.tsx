2817899c8f6df7a19968bb70458461d1b88c72ae
Win Barua <wj.barua@gmail.com>
2025-08-30 22:51:24 +0100

    feat(footer): create footer section (#6)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Merriweather, PT_Sans, Poppins } from "next/font/google";
 import "app/globals.css";
 import Navbar from "components/Navbar";
+import Footer from "@/components/layout/Footer";
 
 const merriweather = Merriweather({
   variable: "--font-merriweather",
@@ -41,6 +42,7 @@ export default function RootLayout({
       >
         <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
